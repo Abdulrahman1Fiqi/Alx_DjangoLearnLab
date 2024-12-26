a9e4b7011149c181b094f9c8ab0ae41e3de80b48
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-26 20:41:26 +0200

    make changes

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -2,6 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth import get_user_model
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password, date_of_birth=None, profile_photo=None):
+        if not username:
+            raise ValueError('The Username must be set')
+        user = self.model(username=username, date_of_birth=date_of_birth, profile_photo=profile_photo)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password, date_of_birth=None, profile_photo=None):
+        user = self.create_user(username, password, date_of_birth, profile_photo)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    objects = CustomUserManager()
+    def __str__(self):
+        return self.username
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
@@ -61,3 +87,9 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+
+class SomeModel(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
