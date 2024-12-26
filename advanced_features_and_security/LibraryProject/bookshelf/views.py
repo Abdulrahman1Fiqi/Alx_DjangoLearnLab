0722b4cf0e29283fa0a06d82a457aa809a8b082c
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-26 21:32:28 +0200

    make changes

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 
+
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
