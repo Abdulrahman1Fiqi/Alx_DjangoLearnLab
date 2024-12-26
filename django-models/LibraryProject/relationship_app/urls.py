78ab9bb16c557df5581e84d1da60d89d8e565a5e
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-26 19:49:08 +0200

    make changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
@@ -9,4 +11,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('', views.list_books, name='home'),  # Example home page view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
