82c2a5f15d8cc53fd934c76a560442e760c64bfd
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-26 19:39:51 +0200

    make changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+from .views import list_books
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relationship_app/', include('relationship_app.urls')),  # Include app URLs
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
