c901ba1dd6befed85d490af35f736a1adc3ecf88
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-27 13:19:53 +0200

    make changes

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path , include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from .views import BookViewSet, CustomAuthToken
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -11,4 +12,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path('', include(router.urls)), 
+    path('auth/token/', CustomAuthToken.as_view(), name='api-token-auth'),  # Token retrieval endpoint
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,6 +8,13 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework.permissions import IsAuthenticated
+
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Retrieve all book instances
     serializer_class = BookSerializer  # Use the BookSerializer to serialize the data
@@ -15,4 +22,15 @@ class BookList(generics.ListAPIView):
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  # Retrieve all book instances
-    serializer_class = BookSerializer  # Use the BookSerializer to serialize the data
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]   # Use the BookSerializer to serialize the data
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
