e816ca68e60e88de421657dca9ecd3534a01bec2
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-27 15:35:49 +0200

    make changes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter,  SearchFilter
 from django_filters import rest_framework
+import filters.OrderingFilter
 
 
 class BookListView(generics.ListCreateAPIView):
