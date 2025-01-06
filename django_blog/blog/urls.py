a2630bf5db1d3d2b76abad7a4fee111df9425994
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2025-01-06 13:36:37 +0200

    make changes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from .views import post_list , register, user_login, user_logout, profile
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
-    path('posts/', post_list, name='post_list'), 
+    path('', post_list, name='post_list'),  # Home page
     path('register/', register, name='register'),  # Registration page
     path('login/', user_login, name='login'),  # Login page
     path('logout/', user_logout, name='logout'),  # Logout page
-    path('profile/', profile, name='profile'),
+    path('profile/', profile, name='profile'),  # User profile page
 ]
