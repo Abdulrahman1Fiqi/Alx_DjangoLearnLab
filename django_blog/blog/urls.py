bec4cdbf2fac32be77256752a768322ce4ef1e53
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2025-01-06 13:04:43 +0200

    make changes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,10 @@
 # blog/urls.py
 from django.urls import path
-from .views import post_list
+from .views import post_list , register, user_login, user_logout, profile
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('posts/', post_list, name='post_list'), 
+    path('register/', register, name='register'),
+    path('login/', user_login),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Post
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+from django.contrib import messages
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'post_list.html', {'posts': posts})
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registration successful.')
+            return redirect('post_list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('post_list')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'registration/login.html')
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('post_list')
+
+@login_required
+def profile(request):
+    return render(request, 'registration/profile.html')
