a1058a75b57fc514e77a48e5e053e39769e33807
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2025-01-11 19:26:35 +0200

    make changes

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created. You can now log in!')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
