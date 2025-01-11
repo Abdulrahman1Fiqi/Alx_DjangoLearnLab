9ef19a4fbbdb50475b8eeefc5485c85da407d010
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2025-01-11 16:41:52 +0200

    make changes

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,17 +1,36 @@
 from rest_framework import serializers
-from .models import CustomUser 
+from .models import CustomUser  
 from django.contrib.auth import authenticate
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user registration and profile management.
+    """
     class Meta:
-        model = CustomUser 
-        fields = ('id', 'username', 'email', 'bio', 'profile_picture')
+        model = CustomUser  
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        """
+        Create a new user with hashed password and return the user.
+        """
+        user = CustomUser (**validated_data)
+        user.set_password(validated_data['password'])  # Hash the password
+        user.save()
+        return user
 
 class LoginSerializer(serializers.Serializer):
+    """
+    Serializer for user login.
+    """
     username = serializers.CharField()
     password = serializers.CharField()
 
     def validate(self, attrs):
+        """
+        Validate the user credentials.
+        """
         user = authenticate(**attrs)
         if user is None:
             raise serializers.ValidationError('Invalid credentials')
